abd95dce35e969ed553c389544a8b897c734a6e5
seyedmojtabazakerialamshiri <seyedmojtabazakerialamshiri@gmail.com>
2021-02-03 00:05:18 +0100

    Add project files.

---
+++ Quartz.Demo/HelloJob.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System;
+using System.Threading.Tasks;
+
+namespace Quartz.Demo
+{
+    public class HelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            var agentId = context.JobDetail.JobDataMap.GetString("agentId");
+            var jobGuid = Guid.Parse(context.JobDetail.Key.Name);
+            await Console.Out.WriteLineAsync($"Agent guid is {agentId} and Job guid is {jobGuid}");
+        }
+    }
+}
---
+++ Quartz.Demo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Quartz.Impl;
+
+namespace Quartz.Demo
+{
+    class Program
+    {
+        /// <summary>
+        /// https://www.freeformatter.com/cron-expression-generator-quartz.html
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static async Task Main(string[] args)
+        {
+            var jobGuid = Guid.NewGuid().ToString();
+
+            var factory = new StdSchedulerFactory();
+            var scheduler = await factory.GetScheduler();
+
+            await scheduler.Start();
+
+            var job = JobBuilder.Create<HelloJob>()
+                .WithIdentity(jobGuid).UsingJobData("agentId", "123")
+                .Build();
+
+            var trigger = TriggerBuilder.Create().StartNow()
+                .WithCronSchedule("0 * * ? * *")
+                .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+
+            Console.ReadLine();
+            Console.WriteLine("Press any key to close the application");
+
+            await scheduler.Shutdown();
+
+        }
+    }
+}
